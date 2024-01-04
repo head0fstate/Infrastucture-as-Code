f6bf3dd6a5f15ad4d317373290df3724b24e453c
head0fstate <151073706+head0fstate@users.noreply.github.com>
2024-01-04 17:47:51 +0100

    Update import requests.py

--- import requests.py
+++ import requests.py
@@ -1,19 +1,23 @@
-import requests
+import time
 
-def check_server_health(url):
-    try:
-        response = requests.get(url)
-        response.raise_for_status()  # Raise an HTTPError for bad responses
-        print(f"Server is healthy. Status code: {response.status_code}")
-    except requests.HTTPError as err:
-        print(f"Server is unhealthy. Error: {err}")
-    except Exception as e:
-        print(f"An unexpected error occurred: {e}")
+import redis
+from flask import Flask
 
-if __name__ == "__main__":
-    # Using Google's homepage as an example
-    server_url = 'https://www.google.com'
-    
-    print(f"Checking the health of the server at {server_url}")
-    
-    check_server_health(server_url)
+app = Flask(__name__)
+cache = redis.Redis(host='redis', port=6379)
+
+def get_hit_count():
+    retries = 5
+    while True:
+        try:
+            return cache.incr('hits')
+        except redis.exceptions.ConnectionError as exc:
+            if retries == 0:
+                raise exc
+            retries -= 1
+            time.sleep(0.5)
+
+@app.route('/')
+def hello():
+    count = get_hit_count()
+    return 'Hello World! I have been seen {} times.\n'.format(count)
